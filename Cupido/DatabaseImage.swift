f3fab6633e7ac155ca69bec3cbab47fb1af03852
Qsunnx <qsunnx@gmail.com>
2022-01-22 14:17:40 +0300

    каркас приложения

---
+++ Cupido/DatabaseImage.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseImage.swift
+//  Cupido
+//
+//  Created by Kirill Yudin on 29.12.2021.
+//
+
+import Foundation
---
+++ Cupido/ImageType.swift
@@ -0,0 +1,8 @@
+//
+//  ImageType.swift
+//  Cupido
+//
+//  Created by Kirill Yudin on 29.12.2021.
+//
+
+import Foundation
